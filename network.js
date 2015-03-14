1611f6d4aab2e1002fed0845228f438feaae9ab9
Nick Monzillo <nickmonzillo@gmail.com>
2015-03-13 21:59:31 -0400

    Initial Commit

---
+++ network.js
@@ -0,0 +1,575 @@
+
+var links = [
+  {
+    "Source":"Four Year Strong",
+    "Target":"Blink 182",
+    "Relate":"0"
+  },
+  {
+    "Source":"Four Year Strong",
+    "Target":"Senses Fail",
+    "Relate":"0"
+  },
+  {
+    "Source":"Four Year Strong",
+    "Target":"A Day to Remember",
+    "Relate":"0"
+  },
+  {
+    "Source":"Four Year Strong",
+    "Target":"Story of the Year",
+    "Relate":"0"
+  },
+  {
+    "Source":"Four Year Strong",
+    "Target":"Brand New",
+    "Relate":"0"
+  },
+  {
+    "Source":"Blink 182",
+    "Target":"Sum 41",
+    "Relate":"1"
+  },
+  {
+    "Source":"Blink 182",
+    "Target":"Sublime",
+    "Relate":"0"
+  },
+  {
+    "Source":"Blink 182",
+    "Target":"Lostprophets",
+    "Relate":"0"
+  },
+  {
+    "Source":"Blink 182",
+    "Target":"Semisonic",
+    "Relate":"0"
+  },
+  {
+    "Source":"Blink 182",
+    "Target":"Simple Plan",
+    "Relate":"0"
+  },
+  {
+    "Source":"Senses Fail",
+    "Target":"Silverstein",
+    "Relate":"0"
+  },
+  {
+    "Source":"Senses Fail",
+    "Target":"Armor for Sleep",
+    "Relate":"0"
+  },
+  {
+    "Source":"Senses Fail",
+    "Target":"Blink 182",
+    "Relate":"0"
+  },
+  {
+    "Source":"Senses Fail",
+    "Target":"The Red Jumpsuit Apparatus",
+    "Relate":"0"
+  },
+  {
+    "Source":"Senses Fail",
+    "Target":"Amber Pacific",
+    "Relate":"0"
+  },
+  {
+    "Source":"A Day to Remember",
+    "Target":"Silverstein",
+    "Relate":"1"
+  },
+  {
+    "Source":"A Day to Remember",
+    "Target":"We Came as Romans",
+    "Relate":"0"
+  },
+  {
+    "Source":"A Day to Remember",
+    "Target":"Story of the Year",
+    "Relate":"0"
+  },
+  {
+    "Source":"A Day to Remember",
+    "Target":"All Time Low",
+    "Relate":"0"
+  },
+  {
+    "Source":"A Day to Remember",
+    "Target":"Escape the Fate",
+    "Relate":"1"
+  },
+  {
+    "Source":"Story of the Year",
+    "Target":"Yellowcard",
+    "Relate":"0"
+  },
+  {
+    "Source":"Story of the Year",
+    "Target":"The Used",
+    "Relate":"0"
+  },
+  {
+    "Source":"Story of the Year",
+    "Target":"Rise Against",
+    "Relate":"0"
+  },
+  {
+    "Source":"Story of the Year",
+    "Target":"Blink 182",
+    "Relate":"0"
+  },
+  {
+    "Source":"Story of the Year",
+    "Target":"The Downtown Fiction",
+    "Relate":"0"
+  },
+  {
+    "Source":"Brand New",
+    "Target":"Taking Back Sunday",
+    "Relate":"0"
+  },
+  {
+    "Source":"Brand New",
+    "Target":"Say Anything",
+    "Relate":"0"
+  },
+  {
+    "Source":"Brand New",
+    "Target":"Band of Horses",
+    "Relate":"0"
+  },
+  {
+    "Source":"Brand New",
+    "Target":"The Used",
+    "Relate":"0"
+  },
+  {
+    "Source":"Brand New",
+    "Target":"Jimmy Eat World",
+    "Relate":"0"
+  },
+  {
+    "Source":"The Used",
+    "Target":"Rise Against",
+    "Relate":"0"
+  },
+  {
+    "Source":"The Used",
+    "Target":"Taking Back Sunday",
+    "Relate":"0"
+  },
+  {
+    "Source":"The Used",
+    "Target":"My Chemical Romance",
+    "Relate":"0"
+  },
+  {
+    "Source":"The Used",
+    "Target":"Panic at the Disco",
+    "Relate":"0"
+  },
+  {
+    "Source":"The Used",
+    "Target":"Blink 182",
+    "Relate":"0"
+  },
+  {
+    "Source":"Sum 41",
+    "Target":"Rise Against",
+    "Relate":"0"
+  },
+  {
+    "Source":"Sum 41",
+    "Target":"Lit",
+    "Relate":"0"
+  },
+  {
+    "Source":"Sum 41",
+    "Target":"Green Day",
+    "Relate":"0"
+  },
+  {
+    "Source":"Sum 41",
+    "Target":"Blink 182",
+    "Relate":"1"
+  },
+  {
+    "Source":"Sum 41",
+    "Target":"American Hi-Fi",
+    "Relate":"0"
+  },
+  {
+    "Source":"Rise Against",
+    "Target":"Anberlin",
+    "Relate":"0"
+  },
+  {
+    "Source":"Rise Against",
+    "Target":"Alien Ant Farm",
+    "Relate":"0"
+  },
+  {
+    "Source":"Rise Against",
+    "Target":"Paramore",
+    "Relate":"0"
+  },
+  {
+    "Source":"Rise Against",
+    "Target":"Fall Out Boy",
+    "Relate":"0"
+  },
+  {
+    "Source":"Rise Against",
+    "Target":"Breaking Benjamin",
+    "Relate":"0"
+  },
+  {
+    "Source":"Taking Back Sunday",
+    "Target":"Spin Doctors",
+    "Relate":"0"
+  },
+  {
+    "Source":"Taking Back Sunday",
+    "Target":"Escape the Fate",
+    "Relate":"0"
+  },
+  {
+    "Source":"Taking Back Sunday",
+    "Target":"Eve 6",
+    "Relate":"0"
+  },
+  {
+    "Source":"Taking Back Sunday",
+    "Target":"Rise Against",
+    "Relate":"0"
+  },
+  {
+    "Source":"Taking Back Sunday",
+    "Target":"Eagle-Eye Cherry",
+    "Relate":"0"
+  },
+  {
+    "Source":"Escape the Fate",
+    "Target":"Falling in Reverse",
+    "Relate":"0"
+  },
+  {
+    "Source":"Escape the Fate",
+    "Target":"Bullet for my Valentine",
+    "Relate":"0"
+  },
+  {
+    "Source":"Escape the Fate",
+    "Target":"Silverstein",
+    "Relate":"0"
+  },
+  {
+    "Source":"Escape the Fate",
+    "Target":"A Day to Remember",
+    "Relate":"1"
+  },
+  {
+    "Source":"Escape the Fate",
+    "Target":"Saosin",
+    "Relate":"0"
+  },
+  {
+    "Source":"Silverstein",
+    "Target":"Underoath",
+    "Relate":"0"
+  },
+  {
+    "Source":"Silverstein",
+    "Target":"The Used",
+    "Relate":"0"
+  },
+  {
+    "Source":"Silverstein",
+    "Target":"Amber Pacific",
+    "Relate":"0"
+  },
+  {
+    "Source":"Silverstein",
+    "Target":"A Day to Remember",
+    "Relate":"1"
+  },
+  {
+    "Source":"Silverstein",
+    "Target":"Saosin",
+    "Relate":"0"
+  },
+  {
+    "Source":"Amber Pacific",
+    "Target":"New Found Glory",
+    "Relate":"0"
+  },
+  {
+    "Source":"Amber Pacific",
+    "Target":"Mayday Parade",
+    "Relate":"0"
+  },
+  {
+    "Source":"Amber Pacific",
+    "Target":"We the Kings",
+    "Relate":"0"
+  },
+  {
+    "Source":"Amber Pacific",
+    "Target":"Hit the Lights",
+    "Relate":"0"
+  },
+  {
+    "Source":"Amber Pacific",
+    "Target":"Yellowcard",
+    "Relate":"0"
+  },
+  {
+    "Source":"Yellowcard",
+    "Target":"Simple Plan",
+    "Relate":"0"
+  },
+  {
+    "Source":"Yellowcard",
+    "Target":"Fall Out Boy",
+    "Relate":"0"
+  },
+  {
+    "Source":"Yellowcard",
+    "Target":"Blink 182",
+    "Relate":"0"
+  },
+  {
+    "Source":"Yellowcard",
+    "Target":"Sum 41",
+    "Relate":"0"
+  },
+  {
+    "Source":"Yellowcard",
+    "Target":"All Time Low",
+    "Relate":"0"
+  },
+  {
+    "Source":"Fall Out Boy",
+    "Target":"Green Day",
+    "Relate":"0"
+  },
+  {
+    "Source":"Fall Out Boy",
+    "Target":"Linkin Park",
+    "Relate":"0"
+  },
+  {
+    "Source":"Fall Out Boy",
+    "Target":"A Day to Remember",
+    "Relate":"0"
+  },
+  {
+    "Source":"Fall Out Boy",
+    "Target":"Panic at the Disco",
+    "Relate":"1"
+  },
+  {
+    "Source":"Fall Out Boy",
+    "Target":"The Red Jumpsuit Apparatus",
+    "Relate":"0"
+  },
+  {
+    "Source":"Panic at the Disco",
+    "Target":"Fall Out Boy",
+    "Relate":"1"
+  },
+  {
+    "Source":"Panic at the Disco",
+    "Target":"My Chemical Romance",
+    "Relate":"1"
+  },
+  {
+    "Source":"Panic at the Disco",
+    "Target":"Paramore",
+    "Relate":"0"
+  },
+  {
+    "Source":"Panic at the Disco",
+    "Target":"Evanescence",
+    "Relate":"0"
+  },
+  {
+    "Source":"Panic at the Disco",
+    "Target":"Cartel",
+    "Relate":"0"
+  },
+  {
+    "Source":"My Chemical Romance",
+    "Target":"Panic at the Disco",
+    "Relate":"1"
+  },
+  {
+    "Source":"My Chemical Romance",
+    "Target":"Green Day",
+    "Relate":"0"
+  },
+  {
+    "Source":"My Chemical Romance",
+    "Target":"AFI",
+    "Relate":"0"
+  },
+  {
+    "Source":"My Chemical Romance",
+    "Target":"Cage the Elephant",
+    "Relate":"0"
+  },
+  {
+    "Source":"My Chemical Romance",
+    "Target":"All American Rejects",
+    "Relate":"0"
+  },
+  {
+    "Source":"Green Day",
+    "Target":"Jimmy Eat World",
+    "Relate":"0"
+  },
+  {
+    "Source":"Green Day",
+    "Target":"All American Rejects",
+    "Relate":"0"
+  },
+  {
+    "Source":"Green Day",
+    "Target":"Weezer",
+    "Relate":"0"
+  },
+  {
+    "Source":"Green Day",
+    "Target":"Red Hot Chili Peppers",
+    "Relate":"0"
+  },
+  {
+    "Source":"Green Day",
+    "Target":"Finger Eleven",
+    "Relate":"0"
+  },
+  {
+    "Source":"Jimmy Eat World",
+    "Target":"Third Eye Blind",
+    "Relate":"0"
+  },
+  {
+    "Source":"Jimmy Eat World",
+    "Target":"Dashboard Confessional",
+    "Relate":"0"
+  },
+  {
+    "Source":"Jimmy Eat World",
+    "Target":"The Ataris",
+    "Relate":"0"
+  },
+  {
+    "Source":"Jimmy Eat World",
+    "Target":"The Postal Service",
+    "Relate":"0"
+  },
+  {
+    "Source":"Jimmy Eat World",
+    "Target":"Blink 182",
+    "Relate":"0"
+  },
+  {
+    "Source":"Simple Plan",
+    "Target":"Green Day",
+    "Relate":"0"
+  },
+  {
+    "Source":"Simple Plan",
+    "Target":"Nine Days",
+    "Relate":"0"
+  },
+  {
+    "Source":"Simple Plan",
+    "Target":"The Click Five",
+    "Relate":"0"
+  },
+  {
+    "Source":"Simple Plan",
+    "Target":"Good Charlotte",
+    "Relate":"0"
+  },
+  {
+    "Source":"Simple Plan",
+    "Target":"The Script",
+    "Relate":"0"
+  }
+]
+
+var nodes = {};
+
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+  link.source = nodes[link.Source] || (nodes[link.Source] = {name: link.Source});
+  link.target = nodes[link.Target] || (nodes[link.Target] = {name: link.Target});
+  link.relate = nodes[link.Relate] || (nodes[link.Relate] = {name: link.Relate});
+});
+
+var width = 1920,
+    height = 950;
+
+
+var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    .linkDistance(50)
+    .charge(-1500)
+    .on("tick", tick)
+    .start();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var link = svg.selectAll(".link")
+    .data(force.links())
+	.enter().append("line")
+    .attr("class", "link")
+
+
+var node = svg.selectAll(".node")
+    .data(force.nodes())
+	.enter().append("g")
+    .attr("class", "node")
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout)
+    .call(force.drag);
+
+node.append("circle")
+    .attr("r", 8);
+
+node.append("text")
+    .attr("x", 15)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.name; });
+
+function tick() {
+  link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; })
+	  .attr("stroke-width", function(d){if(d.Relate == "1") {return 5;} else {return 1.5;} });
+
+  node
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+}
+
+function mouseover() {
+  d3.select(this).select("circle").transition()
+      .duration(750)
+      .attr("r", 16);
+}
+
+function mouseout() {
+  d3.select(this).select("circle").transition()
+      .duration(750)
+      .attr("r", 8);
+}
